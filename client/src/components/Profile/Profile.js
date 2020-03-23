53ef81454798eeba59dc0225e5d7d26f4cbc6ad7
JunjPan <elisepjj@hotmail.com>
2020-03-22 20:13:27 -0700

    add useTransition and anmiated in the profile

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Spring } from 'react-spring/renderprops';
+import { useTransition, animated } from 'react-spring';
 import profileImage from '../../assets/default.jpeg';
 
 function Profile() {
+  const [newArray, setNewArray] = useState([
+    { key: 1, name: 'Github', link: 'https://github.com/Junjpan' },
+  ]);
+
+  function Animation(items) {
+    const transitions = useTransition(items, item => item.key, {
+      from: { transform: 'translate3d(0,-200px,0)' },
+      enter: { transform: 'translate3d(0,0px,0)' },
+      config: { duration: 1500 },
+    });
+
+    return transitions.map(({ item, key, props }) => {
+      return (
+        <animated.p key={key} style={{ color: 'black', fontSize: '1.5rem', ...props }}>
+          {item.name}
+          {' : '}
+          <a href={item.link} target="_blank" rel="noopener noreferrer">
+            {item.link}
+          </a>
+        </animated.p>
+      );
+    });
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNewArray([
+        ...newArray,
+        { key: 2, name: 'Linkedin', link: 'https://www.linkedin.com/in/j-pan-b5b07a155/' },
+      ]);
+    }, 2000);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNewArray([...newArray, { key: 3, name: 'CodePen', link: 'https://codepen.io/merrypjj' }]);
+    }, 5000);
+  }, []);
+
   return (
     <div className="profile">
       <div>
         <h1>
           <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} config={{ duration: 1000 }}>
-            {props => <bold style={props}> Hi, I'm Junjun. </bold>}
+            {props => <b style={props}> Hi, I'm Junjun. </b>}
           </Spring>
           <br />
           <Spring
@@ -16,7 +56,7 @@ function Profile() {
             to={{ opacity: 1 }}
             config={{ delay: 1000, duration: 1500 }}
           >
-            {props => <bold style={props}> You can call me June.</bold>}
+            {props => <b style={props}> You can call me June.</b>}
           </Spring>
           <br />
           <Spring
@@ -25,7 +65,7 @@ function Profile() {
             config={{ delay: 2500, duration: 1500 }}
           >
             {props => (
-              <bold style={props}>
+              <b style={props}>
                 I'm a
                 <Spring
                   from={{ color: 'black' }}
@@ -34,7 +74,7 @@ function Profile() {
                 >
                   {prop => <span style={prop}> Frontend Developer/Full Stack Developer</span>}
                 </Spring>
-              </bold>
+              </b>
             )}
           </Spring>
           <br />
@@ -43,13 +83,18 @@ function Profile() {
             to={{ opacity: 1 }}
             config={{ delay: 4000, duration: 1500 }}
           >
-            {props => <bold style={props}> currenttly living in Orange County, CA .</bold>}
+            {props => <b style={props}> currenttly living in Orange County, CA .</b>}
           </Spring>
         </h1>
-
-        <h4>
-          <a href="https://www.linkedin.com/in/j-pan-b5b07a155/">Linkedin Profile</a>
-        </h4>
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} config={{ delay: 5500, duration: 1500 }}>
+          {props => (
+            <p style={{ color: 'black', fontSize: '1.5rem', ...props }}>
+              {' '}
+              Contact: merrypjj@yahoo.com
+            </p>
+          )}
+        </Spring>
+        {Animation(newArray)}
       </div>
       <div>
         <img
