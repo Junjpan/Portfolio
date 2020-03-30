120863cdaee74fe33cc63b8d31b57c470d94cc92
JunjPan <elisepjj@hotmail.com>
2020-03-29 20:13:12 -0700

    used verifyToken as middleware to the project add route

--- Routes/projectRoute.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express').Router();
-const verifyToken = require('./verifyToken');
-
-Router.get('/edit', verifyToken, (req, res) => {
-  res.send('success');
-});
-
-module.exports = Router;
--- Routes/verifyToken.js
+++
@@ -1,5 +0,0 @@
-const verifyToken = (req, res, next) => {
-  console.log(req.headers['Authentication']);
-};
-
-module.exports = verifyToken;
